707f447cdc821ea636c29cbe97f7ea5e0ea10544
Erik Perin <erikperin.photo@gmail.com>
2016-09-06 11:28:05 -0300

    Updates

---
+++ api/sec/head_spoof.php
@@ -0,0 +1,7 @@
+<?php
+//clear show erros
+error_reporting(0);
+
+//Spoof PHP as ASP.NET and Windows (iak iak iak) :D
+header('X-Powered-By: ASP.NET');
+header('Server: IIS 7 (Windows)');
--- form/test_city_email.php
+++ form/test_city_email.php
@@ -1,5 +1,15 @@
-<form name="testemail" method="post" action="../api/mail/valid_email.php">
-  <input name="uf" type="text">
-  <input name="city" type="text">
-  <button name="send" type="submit">Send</button>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Form Email Tester</title>
+</head>
+<body>
+<form name="testmail" method="post" action="<?php echo htmlentities('../api/mail/valid_email.php'); ?>">
+  <input name="uf" type="text" placeholder="Type the UF">
+  <input name="city" type="text" placeholder="Type the City">
+  <button name="send" type="submit">Send Now</button>
 </form>
+
+</body>
+</html>
