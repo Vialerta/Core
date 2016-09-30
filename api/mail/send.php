7d3a2f0a20f44d1c086d9c0e3a600953ed322d7f
Matheus Blödorn Claudino <blodorn.matheus@gmail.com>
2016-09-30 13:13:53 -0300

    Pass one parameter with properties instead of many parameters to make it friendly

--- api/mail/send.php
+++ api/mail/send.php
@@ -4,11 +4,11 @@ function mailsender($email)
 {
 
 //validate email format
-if (filter_var($email, FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL)) {
+if (filter_var($email['address'], FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL)) {
     // send te correct input values
-    $to = $email; //get the TO email
-    $subject = ''; //The subject to send
-    $txt = ''; //Content and Body of Email (import from template)
+    $to = $email['to']; //get the TO email
+    $subject = $email['subject']; //The subject to send
+    $txt = $email['txt']; //Content and Body of Email (import from template)
     $headers = 'From: noreply@vialerta.cc'; //Headers
     $headers = 'MIME-Version: 1.0'."\r\n";
     $headers = 'Content-type:text/html;charset=UTF-8'."\r\n";
@@ -16,6 +16,6 @@ if (filter_var($email, FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL)) {
     //send all data
     mail($to, $subject, $message, $headers);
 } else {
-    die ("The ($email) email its wrong"); //if wrong show error
+    die ("The ($email) email is wrong"); //if wrong show error
 }
 }
