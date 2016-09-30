658a4a90bebd6763de0bb4993e6fbd5c45ffb0ce
Erik Perin <erikperin.photo@gmail.com>
2016-09-30 17:54:40 -0300

    Issue Mail Template improvement

--- view/mail/alerta.tpl.php
+++ view/mail/alerta.tpl.php
@@ -1,8 +1,6 @@
 <?php
-
 $set_default_url = "http://$_SERVER[HTTP_HOST]";
-
- ?>
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -23,6 +21,7 @@ $set_default_url = "http://$_SERVER[HTTP_HOST]";
 
 <style>
 /* Basics */
+
 body {
     margin: 0 !important;
     padding: 0;
@@ -32,8 +31,7 @@ table {
     border-spacing: 0;
     font-family: sans-serif;
     background-color: white;
-    color: #333333;
-
+    color: #686c70;
 }
 td {
     padding: 0;
@@ -42,7 +40,7 @@ img {
     border: 0;
 }
 div[style*="margin: 16px 0"] {
-    margin:0 !important;
+    margin: 0 !important;
 }
 .wrapper {
     width: 100%;
@@ -55,29 +53,29 @@ div[style*="margin: 16px 0"] {
     margin: 0 auto;
 }
 .outer {
-margin: 0 auto;
+    margin: 0 auto;
     width: 100%;
     max-width: 600px;
 }
-
 .full-width-image img {
     width: 100%;
     max-width: 600px;
     height: auto;
     border-radius: 2px;
 }
-
 .inner {
-padding: 10px;
+    padding: 10px;
 }
 p {
     margin: 0;
+    font-size: 0.9em;
     line-height: 1.5em;
     text-align: justify;
+    color: #686c70;
 }
 a {
-    color: #ee6a56;
-    text-decoration: underline;
+    color: #013D5F;
+    text-decoration: none;
 }
 .h1 {
     font-size: 21px;
@@ -86,11 +84,11 @@ a {
 }
 .h2 {
     font-size: 18px;
-    font-weight: bold;
-    Margin-bottom: 12px;
+    font-weight: lighter;
+    margin-bottom: 12px;
 }
-
 /* One column layout */
+
 .one-column .contents {
     text-align: left;
 }
@@ -98,69 +96,131 @@ a {
     font-size: 14px;
     margin-bottom: 10px;
 }
-.rodape{
+.rodape {
     background-color: #fff;
     color: grey;
     font-size: 0.8em;
     text-align: center;
 }
-.button{
+.button {
     text-align: center;
     margin: 0 auto;
     background-color: #013D5F;
-    border:none;
+    border: none;
     font-size: 1.0em;
     border-radius: 2px;
     color: white;
-    padding:  10px 15px;
+    padding: 10px 15px;
+    cursor: pointer;
 }
 hr {
     box-sizing: content-box;
     height: 0;
-    margin: 40px 0;
+    margin: 30px 0;
     border: 0;
-    border-top: 1px solid #ddd;
+    border-top: 1px  solid #ddd;
+}
+
+.card{
+    padding: 10px;
+    margin: 5px;
+    margin-top: 10px;
+
+    border:1px  solid rgba(128, 128, 128, 0.1);
+
+}
+
+.caption{
+    font-size: .8em;
+}
+
+dt{
+    font-weight: bold;
+    border-bottom:1px solid #f1f2f3;
+    margin-bottom: 10px;
+    margin-top: 12px;
+    font-size: .8em;
+}
+dd {
+	background:none no-repeat left top;
+	padding-left: 50px;
 }
 </style>
 
 </head>
 <body>
-    <center class="wrapper">
-        <div class="webkit">
-            <table class="outer" align="center">
-                <tr>
-                <td class="inner contents" style="text-align:center;">
-                    <img width="100" src="<?php echo $set_default_url ?>/core/view/mail/assets/logo.png" >
-                </td>
+   <center class="wrapper">
+      <div class="webkit">
+         <table class="outer" align="center">
+            <tr>
+               <td class="inner contents" style="text-align:center;">
+                  <img width="100" src="<?php echo $set_default_url ?>/core/view/mail/assets/logo.png" >
+               </td>
             </tr>
-                <tr>
-                   <td class="full-width-image">
-                       <img src="http://placehold.it/1359x377" width="600" alt="" />
-                   </td>
-               </tr>
+            <!-- <tr>
+               <td class="full-width-image">
+                  <img src="http://placehold.it/1359x377" width="600" alt="" />
+               </td>
+            </tr> -->
+
+
+            <tr>
+               <td class="full-width-image">
+                  <div>
+                  <img src="http://placehold.it/800x600" width="900" alt="" /><br /><br />
+
+                </div>
+               </td>
+            </tr>
+
+
+            <tr>
+               <td class="inner contents">
+
+                  <p class="h1">Lorem ipsum dolor sit amet</p>
+                  <p>Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent laoreet malesuada cursus. Maecenas scelerisque congue eros eu posuere. Praesent in felis ut velit pretium lobortis rhoncus ut erat.</p><br />
+               </td>
+            </tr>
+
+            <tr>
+               <td class="inner contents card">
+
+                 <p class="h2">Data</p>
+                  <dl>
+                      <dt>#Identity</dt>
+                      <dd><a href="">US NY 321456 AB</a></dd>
+                      <dt>Location</dt>
+                      <dd><a href="">Wall Street, 1042</a> - <a href="">New York</a> - <a href="">NY</a> - <a href="">USA</a></dd>
+                      <dt>Type</dt>
+                      <dd><a href="">Structure</a></dd>
+                      <dt>User</dt>
+                      <dd><a href="">@johndoe</a></dd>
+                      <dt>Date</dt>
+                      <dd><a href="">January, 6 2016</a></dd>
+                    </dl>
+               </td>
+            </tr>
+
+
+            <tr>
+               <td class="inner contents"><br />
+                  <a href="#"><button class="button">View Issue</button></a>
+              </td>
+            </tr>
 
 
 
             <tr>
-            <td class="inner contents"><br />
-                <p class="h1">Lorem ipsum dolor sit amet</p>
-                <p>Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent laoreet malesuada cursus. Maecenas scelerisque congue eros eu posuere. Praesent in felis ut velit pretium lobortis rhoncus ut erat.</p><br />
-
-            </td>
-        </tr>
-        <tr>
-        <td class="inner contents">
-                <button class="button">Sign In</button>
-        </td>
-    </tr>
-        <tr>
-        <td class="inner contents rodape">
-
-            <p>rodape aqui</p>
-        </td>
-    </tr>
-            </table>
-        </div>
-    </center>
+               <td class="inner contents rodape">
+                  <hr />
+                  <p><a href="http://vialerta.cc">Vialerta</a> - Crowdsourcing ERP for Cities</p>
+                  <p>
+                     <small>Make yourself heard and covers their rights for a better city.</small>
+                  </p>
+               </td>
+            </tr>
+         </table>
+      </div>
+   </center>
 </body>
 </html>
