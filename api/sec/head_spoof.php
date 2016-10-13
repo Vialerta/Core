8829bdbeb1cd63dbcf5915f49d034f4d972218de
Erik Perin <erikperin.photo@gmail.com>
2016-10-13 15:32:30 -0300

    Mods on Spoof File
    add a random server name and language

--- api/sec/head_spoof.php
+++ api/sec/head_spoof.php
@@ -1,8 +1,31 @@
 <?php
+
+
+/*
+       _                    _____                                                   ___
+      | |                  (_____)                                                 / __)
+ _ _ _| | _   ____ ____       _       ___  ____ ____    _   _  ___  _   _  ____   | |__ ____  ____ ____
+| | | | || \ / _  )  _ \     | |     /___)/ _  ) _  )  | | | |/ _ \| | | |/ ___)  |  __) _  |/ ___) _  )
+| | | | | | ( (/ /| | | |   _| |_   |___ ( (/ ( (/ /   | |_| | |_| | |_| | |      | | ( ( | ( (__( (/ / _ _ _
+ \____|_| |_|\____)_| |_|  (_____)  (___/ \____)____)   \__  |\___/ \____|_|      |_|  \_||_|\____)____|_|_|_)
+                                                       (____/
+
+
+*/
+
 //clear show erros
 error_reporting(0);
 
-//Spoof PHP as ASP.NET and Windows (iak iak iak) :D
-header('X-Powered-By: ASP.NET');
-header('Server: IIS 7 (Windows)');
-?>
+$whatsyournameserver = array('OpenBSD', 'Windows NT', 'Ubuntu', 'CentOS', 'Oracle Linux', 'Fedora', 'Debian');
+
+$whatsyournamesystem = array('Kode C ANSII', 'C#', 'Python', 'Ruby', 'Rails', 'ASP.NET', 'Java EE');
+
+  $rand_keys_server = array_rand($whatsyournameserver);
+  $rand_keys_system = array_rand($whatsyournameserver);
+
+  $whatsyournameserver = $whatsyournameserver[$rand_keys_server];
+  $whatsyournamesystem = $whatsyournamesystem[$rand_keys_system];
+
+//Spoof is Good (iak iak iak) :D
+header('X-Powered-By:'.$whatsyournameserver);
+header('Server:'.$whatsyournamesystem);
